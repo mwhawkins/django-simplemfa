e2dbeeb7c38be2da7b771eed3e3f33e320cc9d39
Mike Hawkins <mhawkins@netizencorp.com>
2020-07-16 17:44:29 -0400

    remove build files

--- build/lib/simplemfa/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from simplemfa.models import AuthCode
-
-
-admin.site.register(AuthCode)
--- build/lib/simplemfa/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SimplemfaConfig(AppConfig):
-    name = 'simplemfa'
