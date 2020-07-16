b0ee91b99c9c6ca2abf75c82f3c99aa955617a6c
Mike Hawkins <mhawkins@netizencorp.com>
2020-07-16 17:06:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
 from setuptools import setup
 
-setup()
+setup(name="django-simplemfa",
+    version="0.1",
+    author="Michael Hawkins",
+    author_email="mhawkins@netizen.net",
+    description="An implementation of basic multi-factor authentication (MFA) for django projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mwhawkins/django-simplemfa",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',)
