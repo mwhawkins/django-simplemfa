531da19774ac01030b8601a75525e9a2c7afd963
Mike Hawkins <mhawkins@netizencorp.com>
2020-07-16 17:14:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name="django-simplemfa",
     version="0.1",
     author="Michael Hawkins",
     author_email="mhawkins@netizen.net",
     description="An implementation of basic multi-factor authentication (MFA) for django projects",
-    long_description=file: README.rst,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mwhawkins/django-simplemfa",
     packages=setuptools.find_packages(),
